39aed9c8adb35bf91ecd6d00888e99cf6b001444
William Plunkett Jr <joey.plunkett92@yahoo.com>
2018-07-30 22:17:42 -0500

    Update __resource.lua

--- __resource.lua
+++ __resource.lua
@@ -1,28 +1,22 @@
 resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
-description 'ESX Boat Shop'
+description 'ESX Aircraft Shop'
 
-version '1.0.0'
+version '1.2.0'
 
 server_scripts {
-  '@mysql-async/lib/MySQL.lua',
-  '@es_extended/locale.lua' ,
-  'locales/br.lua' ,
-  'locales/de.lua' ,
-  'locales/en.lua' ,
-  'locales/fr.lua' ,
-  'locales/sv.lua' ,
-  'config.lua',
-  'server/main.lua'
+	'@mysql-async/lib/MySQL.lua',
+	'@es_extended/locale.lua',
+	'locales/en.lua',
+	'config.lua',
+	'server/main.lua'
 }
 
 client_scripts {
-  '@es_extended/locale.lua' ,
-  'locales/br.lua' ,
-  'locales/de.lua' ,
-  'locales/en.lua' ,
-  'locales/fr.lua' ,
-  'locales/sv.lua' ,
-  'config.lua',
-  'client/main.lua'
+	'@es_extended/locale.lua',
+	'locales/en.lua',
+	'config.lua',
+	'client/utils.lua',
+	'client/main.lua'
 }
+
