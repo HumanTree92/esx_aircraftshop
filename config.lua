146fc3e59a467d48b77d3e0aa7ab16a567d5f59b
William Plunkett Jr <joey.plunkett92@yahoo.com>
2020-02-17 12:42:12 -0600

    Final Commit

--- config.lua
+++ config.lua
@@ -1,13 +1,12 @@
 Config = {}
 Config.Locale = 'en'
 
-Config.DrawDistance = 100.0
-Config.MarkerColor  = { r = 120, g = 120, b = 240 }
+Config.DrawDistance = 100
+Config.MarkerColor  = {r = 120, g = 120, b = 240}
 
-Config.EnableOwnedVehicles = true -- If true then it will set the Vehicle Owner to the Player who bought it.
-Config.ResellPercentage    = 75 -- Sets the Resell Percentage | Example: $100 Car will resell for $75
-Config.LicenseEnable       = true -- Require people to own a Aircraft License when buying Vehicles? Requires esx_license
-Config.LicensePrice        = 25000 -- Sets the License Price if Config.LicenseEnable is true
+Config.ResellPercentage = 50
+Config.LicenseEnable    = true
+Config.LicensePrice     = 25000
 
 -- looks like this: 'LLL NNN'
 -- The maximum plate length is 8 chars (including spaces & symbols), don't go past it!
@@ -17,25 +16,25 @@ Config.PlateUseSpace = true
 
 Config.Zones = {
 	ShopEntering = { -- Marker for Accessing Shop
-		Pos   = { x = -940.06, y = -2963.15, z = 18.85 },
-		Size  = { x = 1.5, y = 1.5, z = 1.0 },
+		Pos   = vector3(-940.0, -2963.1, 18.8),
+		Size  = {x = 1.5, y = 1.5, z = 1.0},
 		Type  = 1
 	},
 	ShopInside = { -- Marker for Viewing Vehicles
-		Pos     = { x = -1874.76, y = -3137.59, z = 14.94 }, -- { x = -1075.01, y = -2933.22, z = 14.55 },
-		Size    = { x = 1.5, y = 1.5, z = 1.0 },
+		Pos     = vector3(-1874.7, -3137.5, 14.9), -- vector3(-1075.0, -2933.2, 14.5),
+		Size    = {x = 1.5, y = 1.5, z = 1.0},
 		Heading = 333.95, -- 59.9
 		Type    = -1
 	},
 	ShopOutside = { -- Marker for Purchasing Vehicles
-		Pos     = { x = -965.25, y = -2983.56, z = 14.55 },
-		Size    = { x = 1.5, y = 1.5, z = 1.0 },
+		Pos     = vector3(-965.2, -2983.5, 14.5),
+		Size    = {x = 1.5, y = 1.5, z = 1.0},
 		Heading = 59.9,
 		Type    = -1
 	},
 	ResellVehicle = { -- Marker for Selling Vehicles
-		Pos   = { x = -1003.24, y = -2920.79, z = 12.95 },
-		Size  = { x = 3.0, y = 3.0, z = 1.0 },
+		Pos   = vector3(-1003.2, -2920.7, 12.9),
+		Size  = {x = 3.0, y = 3.0, z = 1.0},
 		Type  = 1
 	}
 }
--- server/main.lua
+++ server/main.lua
@@ -1,7 +1,5 @@
 ESX = nil
-
-local Categories = {}
-local Vehicles   = {}
+local categories, vehicles = {}, {}
 
 TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
@@ -11,7 +9,7 @@ Citizen.CreateThread(function()
 	if Config.PlateUseSpace then char = char + 1 end
 
 	if char > 8 then
-		print(('esx_aircraftshop: ^1WARNING^7 plate character count reached, %s/8 characters.'):format(char))
+		print(('[esx_aircraftshop] [^3WARNING^7] Plate character count reached, %s/8 characters!'):format(char))
 	end
 end)
 
@@ -22,25 +20,26 @@ function RemoveOwnedVehicle(plate)
 end
 
 MySQL.ready(function()
-	Categories     = MySQL.Sync.fetchAll('SELECT * FROM aircraft_categories')
-	local vehicles = MySQL.Sync.fetchAll('SELECT * FROM aircrafts')
+	MySQL.Async.fetchAll('SELECT * FROM aircraft_categories', {}, function(_categories)
+		categories = _categories
 
-	for i=1, #vehicles, 1 do
-		local vehicle = vehicles[i]
+		MySQL.Async.fetchAll('SELECT * FROM aircrafts', {}, function(_vehicles)
+			vehicles = _vehicles
 
-		for j=1, #Categories, 1 do
-			if Categories[j].name == vehicle.category then
-				vehicle.categoryLabel = Categories[j].label
-				break
+			for k,v in ipairs(vehicles) do
+				for k2,v2 in ipairs(categories) do
+					if v2.name == v.category then
+						vehicles[k].categoryLabel = v2.label
+						break
+					end
+				end
 			end
-		end
 
-		table.insert(Vehicles, vehicle)
-	end
-
-	-- send information after db has loaded, making sure everyone gets vehicle information
-	TriggerClientEvent('esx_aircraftshop:sendCategories', -1, Categories)
-	TriggerClientEvent('esx_aircraftshop:sendVehicles', -1, Vehicles)
+			-- send information after db has loaded, making sure everyone gets vehicle information
+			TriggerClientEvent('esx_aircraftshop:sendCategories', -1, categories)
+			TriggerClientEvent('esx_aircraftshop:sendVehicles', -1, vehicles)
+		end)
+	end)
 end)
 
 ESX.RegisterServerCallback('esx_aircraftshop:buyLicense', function(source, cb)
@@ -58,94 +57,96 @@ ESX.RegisterServerCallback('esx_aircraftshop:buyLicense', function(source, cb)
 	end
 end)
 
-RegisterServerEvent('esx_aircraftshop:setVehicleOwned')
-AddEventHandler('esx_aircraftshop:setVehicleOwned', function(vehicleProps)
-	local _source = source
-	local xPlayer = ESX.GetPlayerFromId(_source)
+function getVehicleLabelFromModel(model)
+	for k,v in ipairs(vehicles) do
+		if v.model == model then
+			return v.name
+		end
+	end
 
-	MySQL.Async.execute('INSERT INTO owned_vehicles (owner, plate, vehicle, Type) VALUES (@owner, @plate, @vehicle, @Type)',
-	{
-		['@owner']   = xPlayer.identifier,
-		['@plate']   = vehicleProps.plate,
-		['@vehicle'] = json.encode(vehicleProps),
-		['@Type']    = 'aircraft'
-	}, function(rowsChanged)
-		TriggerClientEvent('esx:showNotification', _source, _U('aircraft_belongs', vehicleProps.plate))
-	end)
-end)
+	return
+end
 
 ESX.RegisterServerCallback('esx_aircraftshop:getCategories', function(source, cb)
-	cb(Categories)
+	cb(categories)
 end)
 
 ESX.RegisterServerCallback('esx_aircraftshop:getVehicles', function(source, cb)
-	cb(Vehicles)
+	cb(vehicles)
 end)
 
-ESX.RegisterServerCallback('esx_aircraftshop:buyVehicle', function(source, cb, vehicleModel)
-	local xPlayer     = ESX.GetPlayerFromId(source)
-	local vehicleData = nil
+ESX.RegisterServerCallback('esx_aircraftshop:buyVehicle', function(source, cb, model, plate)
+	local xPlayer = ESX.GetPlayerFromId(source)
+	local modelPrice
 
-	for i=1, #Vehicles, 1 do
-		if Vehicles[i].model == vehicleModel then
-			vehicleData = Vehicles[i]
+	for k,v in ipairs(vehicles) do
+		if model == v.model then
+			modelPrice = v.price
 			break
 		end
 	end
 
-	if xPlayer.getMoney() >= vehicleData.price then
-		xPlayer.removeMoney(vehicleData.price)
-		cb(true)
+	if modelPrice and xPlayer.getMoney() >= modelPrice then
+		xPlayer.removeMoney(modelPrice)
+
+		MySQL.Async.execute('INSERT INTO owned_vehicles (owner, plate, vehicle, type) VALUES (@owner, @plate, @vehicle, @type)', {
+			['@owner']   = xPlayer.identifier,
+			['@plate']   = plate,
+			['@vehicle'] = json.encode({model = GetHashKey(model), plate = plate}),
+			['@type']    = 'aircraft'
+		}, function(rowsChanged)
+			xPlayer.showNotification(_U('aircraft_belongs', plate))
+			cb(true)
+		end)
 	else
 		cb(false)
 	end
 end)
 
 ESX.RegisterServerCallback('esx_aircraftshop:resellVehicle', function(source, cb, plate, model)
-	local resellPrice = 0
+	local resellPrice
 
 	-- calculate the resell price
-	for i=1, #Vehicles, 1 do
-		if GetHashKey(Vehicles[i].model) == model then
-			resellPrice = ESX.Math.Round(Vehicles[i].price / 100 * Config.ResellPercentage)
+	for i=1, #vehicles, 1 do
+		if GetHashKey(vehicles[i].model) == model then
+			resellPrice = ESX.Math.Round(vehicles[i].price / 100 * Config.ResellPercentage)
 			break
 		end
 	end
 
-	if resellPrice == 0 then
-		print(('esx_aircraftshop: %s attempted to sell an unknown vehicle!'):format(GetPlayerIdentifiers(source)[1]))
+	if not resellPrice then
+		print(('[esx_aircraftshop] [^3WARNING^7] %s attempted to sell an unknown vehicle!'):format(GetPlayerIdentifiers(source)[1]))
 		cb(false)
-	end
+	else
+		local xPlayer = ESX.GetPlayerFromId(source)
 
-	local xPlayer = ESX.GetPlayerFromId(source)
-	
-	MySQL.Async.fetchAll('SELECT * FROM owned_vehicles WHERE owner = @owner AND @plate = plate', {
-		['@owner'] = xPlayer.identifier,
-		['@plate'] = plate
-	}, function(result)
-		if result[1] then -- does the owner match?
-			local vehicle = json.decode(result[1].vehicle)
-			if vehicle.model == model then
-				if vehicle.plate == plate then
-					xPlayer.addMoney(resellPrice)
-					RemoveOwnedVehicle(plate)
-					cb(true)
+		MySQL.Async.fetchAll('SELECT * FROM owned_vehicles WHERE owner = @owner AND @plate = plate', {
+			['@owner'] = xPlayer.identifier,
+			['@plate'] = plate
+		}, function(result)
+			if result[1] then -- does the owner match?
+				local vehicle = json.decode(result[1].vehicle)
+
+				if vehicle.model == model then
+					if vehicle.plate == plate then
+						xPlayer.addMoney(resellPrice)
+						RemoveOwnedVehicle(plate)
+						cb(true)
+					else
+						print(('[esx_aircraftshop] [^3WARNING^7] %s attempted to sell an vehicle with plate mismatch!'):format(xPlayer.identifier))
+						cb(false)
+					end
 				else
-					print(('esx_aircraftshop: %s attempted to sell an vehicle with plate mismatch!'):format(xPlayer.identifier))
+					print(('[esx_aircraftshop] [^3WARNING^7] %s attempted to sell an vehicle with model mismatch!'):format(xPlayer.identifier))
 					cb(false)
 				end
-			else
-				print(('esx_aircraftshop: %s attempted to sell an vehicle with model mismatch!'):format(xPlayer.identifier))
-				cb(false)
 			end
-		else
-			cb(false)
-		end
-	end)
+		end)
+	end
 end)
 
 ESX.RegisterServerCallback('esx_aircraftshop:isPlateTaken', function(source, cb, plate)
-	MySQL.Async.fetchAll('SELECT * FROM owned_vehicles WHERE plate = @plate', {
+	MySQL.Async.fetchAll('SELECT 1 FROM owned_vehicles WHERE plate = @plate', {
 		['@plate'] = plate
 	}, function(result)
 		cb(result[1] ~= nil)
@@ -164,7 +165,7 @@ ESX.RegisterServerCallback('esx_aircraftshop:retrieveJobVehicles', function(sour
 	end)
 end)
 
-RegisterServerEvent('esx_aircraftshop:setJobVehicleState')
+RegisterNetEvent('esx_aircraftshop:setJobVehicleState')
 AddEventHandler('esx_aircraftshop:setJobVehicleState', function(plate, state)
 	local xPlayer = ESX.GetPlayerFromId(source)
 
@@ -174,7 +175,7 @@ AddEventHandler('esx_aircraftshop:setJobVehicleState', function(plate, state)
 		['@job'] = xPlayer.job.name
 	}, function(rowsChanged)
 		if rowsChanged == 0 then
-			print(('esx_aircraftshop: %s exploited the garage!'):format(xPlayer.identifier))
+			print(('[esx_aircraftshop] [^3WARNING^7] %s exploited the garage!'):format(xPlayer.identifier))
 		end
 	end)
 end)
